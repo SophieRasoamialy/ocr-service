68d9e019a096228600cb7da4fd8718e34be790a4
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-07-11 11:15:59 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import {  Html, Head} from "next/document";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -43,8 +44,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
+    <Html lang="en">
+      <Head>
         <meta name="description" content="Convert images to text using OCR" />
         <meta name="keywords" content="OCR, Image to Text, Online OCR" />
         <meta name="author" content="Sophie Hasindrae" />
@@ -58,13 +59,13 @@ export default function RootLayout({
         <meta name="twitter:description" content="Convert images to text using OCR" />
         <meta name="twitter:image" content="https://my-image-to-text.vercel.app/itt.gif" />
         <meta name="google-adsense-account" content="ca-pub-4219691541903798"/>
-      </head>
+      </Head>
       <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}>{children}
         <Toaster />
       </body>
-    </html>
+    </Html>
   );
 }
